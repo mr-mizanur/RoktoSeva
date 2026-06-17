6f145ab88f6e08e8a341c875d59086b02fe7dff2
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-06-17 20:40:23 +0600

    add batter-auth and mongodb

---
+++ src/lib/auth-client.js
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/client";
+
+export const authClient = createAuthClient({
+
+  baseURL: process.env.NEXT_PUBLIC_APP_URL
+});
+
+export const { signIn, signUp, useSession } = authClient;
