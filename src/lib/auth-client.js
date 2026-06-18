0ba45ba9fd783453f6d37ba9fc261a3c3f899668
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-06-18 21:07:15 +0600

    fix: update auth client baseURL

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || "http://localhost:3000/", 
+  
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://rokto-seva.vercel.app", 
 });
 
 export const { signIn, signUp, useSession } = authClient;
